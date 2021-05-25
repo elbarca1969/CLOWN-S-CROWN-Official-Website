4a23e400843956f9afca3707b483477588263c7a
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-05-25 19:15:47 +0900

    itemクラスが付与された要素はスクロールが検知したら表示

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  const cb = function (el, isIntersecting) {
+      if(isIntersecting) {
+          el.classList.add('inview');
+      }
+  }
+
+  new ScrollObserver('.item', cb);
+
   new MobileMenu();
 });
